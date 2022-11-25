9c9bc682f06a96339b2f0ea8301c6e0d94ebc646
Yabrok <nosor321@mail.ru>
2022-11-25 14:58:13 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+user = prompt('Ismingizni kiriting');
+test1 = prompt('BS-da nechta col bor?: \n a)24 \n b)12 \n c)6 \n d)8');
+test2 = prompt('Tailwind bu nima?: \n a)Dasturlash tili \n b)CSS-framework \n c)Kutubxona \n d)Preprotsesor');
+test3 = prompt('BEM bu nima?: \n a)Block Element Modified \n b)Element Block \n c)Block Element Modifier \n d)Javob yoq');
+test4 = prompt('CSS ulashni nechta turi bor?: \n a)2 \n b)1 \n c)3 \n d)4');
+test5 = prompt('justify-content ni deafault xolati: \n a)row \n b)space-between \n c)start \n d)end');
+
+
+console.log('User: ' + user + "\n" + "\n" + 
+'Test-1' + "\n\t" + 'user: ' + test1 + "\n\t" +'correct: b' + '\n' +
+'Test-2' + "\n\t" + 'user: ' + test2 + "\n\t" +'correct: b' + '\n' + 
+'Test-3' + "\n\t" + 'user: ' + test3 + "\n\t" +'correct: c' + '\n' +
+'Test-4' + "\n\t" + 'user: ' + test4 + "\n\t" +'correct: c' + '\n' +
+'Test-5' + "\n\t" + 'user: ' + test5 + "\n\t" +'correct: c');
